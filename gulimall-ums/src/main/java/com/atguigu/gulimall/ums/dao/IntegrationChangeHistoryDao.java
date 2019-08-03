c5e17a13beaa9190f427f052621e7b713cf85f80
TuoZaiLee <1416846478@qq.com>
2019-08-03 08:57:12 +0800

    所有模块swagger配置完成以及逆向工程

---
+++ gulimall-ums/src/main/java/com/atguigu/gulimall/ums/dao/IntegrationChangeHistoryDao.java
@@ -0,0 +1,17 @@
+package com.atguigu.gulimall.ums.dao;
+
+import com.atguigu.gulimall.ums.entity.IntegrationChangeHistoryEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 积分变化历史记录
+ * 
+ * @author leifengyang
+ * @email lfy@atguigu.com
+ * @date 2019-08-02 19:46:36
+ */
+@Mapper
+public interface IntegrationChangeHistoryDao extends BaseMapper<IntegrationChangeHistoryEntity> {
+	
+}
